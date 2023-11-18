0961d7c9f319288dbf5de1cf28468ab154bbc0c0
baotrung262 <baotrung262@gmail.com>
2023-11-18 09:56:39 +0700

    NguyenBaoTrung-21810310475

---
+++ insert.php
@@ -0,0 +1,23 @@
+<?php
+/* Cố gắng kết nối máy chủ MySQL. Giả sử bạn đang chạy MySQL
+Máy chủ có cài đặt mặc định (người dùng 'root' không có mật khẩu) */
+$link = mysqli_connect("localhost", "root", "", "thuchanh18112023");
+ 
+// Kiểm tra kết nối
+if($link === false){
+    die("ERROR: Không thể kết nối. " . mysqli_connect_error());
+}
+ 
+// Cố gắng thực hiện câu lệnh INSERT
+$sql = "INSERT INTO myguests (first_name, last_name, email) VALUES ('Trung', 'BAo', 'baotrung222@mail.com')";
+if(mysqli_query($link, $sql)){
+    // Lấy ID đã chèn cuối cùng
+    $last_id = mysqli_insert_id($link);
+    echo "Chèn bản ghi thành công. ID đã chèn cuối cùng là: " . $last_id;
+} else{
+    echo "ERROR: Không thể thực thi câu lệnh $sql. " . mysqli_error($link);
+}
+ 
+// Đóng kết nối
+mysqli_close($link);
+?>
